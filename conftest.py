9a11a9233c8cc88101c018131c5c023294374db3
Vladimir <vvgspok@gmail.com>
2025-06-23 20:53:23 +0300

    Исправил фикстуру courier и добавил authorization_courier

--- conftest.py
+++ conftest.py
@@ -6,7 +6,11 @@ from methods.couriers_method import CourierMethods
 def courier():
     courier_data = generate_couriers_data()
     response = CourierMethods().create_courier(courier_data)
+    yield response, courier_data
     id_courier = CourierMethods().login_courier(courier_data)
-    yield id_courier, response.status_code, response.text, courier_data
     CourierMethods().delete_courier(id_courier.json()['id'])
 
+@pytest.fixture
+def authorization_courier(courier):
+    response = CourierMethods().login_courier(courier[1])
+    return response
